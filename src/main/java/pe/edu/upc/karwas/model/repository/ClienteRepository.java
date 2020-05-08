48d9b5b8ef100bdbcf2e6d408460febbdc341dd7
JuanAlonso00047 <[u201523692@upc.edu.pe]>
2020-05-07 20:49:43 -0500

    Add Imp

--- src/main/java/pe/edu/upc/karwas/model/repository/ClienteRepository.java
+++ src/main/java/pe/edu/upc/karwas/model/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package pe.edu.upc.karwas.model.repository;
 
+
+
 public interface ClienteRepository {
 
 }
---
+++ src/main/java/pe/edu/upc/karwas/model/repository/impl/AutoRepositoryImpl.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.karwas.model.repository.impl;
+
+public class AutoRepositoryImpl {
+
+}
