b082c314e09c6c5fe42c8bdd2df996284edb4491
Sheyla <sheylasabrina94@hotmail.com>
2020-05-07 22:27:26 -0500

    Add client and car classes

---
+++ src/main/java/pe/edu/upc/karwas/model/entity/Client.java
@@ -0,0 +1,63 @@
+package pe.edu.upc.karwas.model.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clients")
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "id_person", length = 10, nullable = false)
+	private Integer id_person;
+	
+	@Column(name = "username", length = 10, nullable = false)
+	private String username;
+	
+	@Column(name = "password", length = 15, nullable = false)
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId_person() {
+		return id_person;
+	}
+
+	public void setId_person(Integer id_person) {
+		this.id_person = id_person;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
+
+
