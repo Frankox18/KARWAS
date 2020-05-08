30757faf9eff3755b3ca0338299797844142ad32
Villa223003 <u201716156@upc.edu.pe>
2020-05-08 14:37:43 -0500

    agregar repository y repository implement

---
+++ src/main/java/pe/edu/upc/karwas/model/repository/JpaRepository.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.karwas.model.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JpaRepository<T, ID> {
+		Integer insert(T t) throws Exception;
+		Integer update(T t) throws Exception;
+		Integer delete(T t) throws Exception;
+		List<T> findAll() throws Exception;
+		Optional<T> findById(ID id) throws Exception;
+
+}
